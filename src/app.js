fefa6774c8f211dddd328f62fc5d1f8d682ceffc
sparks <sparks@localhost.localdomain>
2023-03-20 10:46:44 +0300

    added someresponsiveness

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const open = document.querySelector(".open")
+const close = document.querySelector(".close")
+
+const sidebar = document.querySelector(".sidebar")
+
+
+
+// when the screen size is 
+
+if(window)
+sidebar.style.display = "none"
